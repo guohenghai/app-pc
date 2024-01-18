d719fdf4d97b09207dbf245f97a32bb81802c15a
guohenghai <guohenghai@126.com>
2024-01-18 17:15:21 +0800

    提交无用的修改

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,12 @@
+/*
+ * @Author       : 郭恒海
+ * @Date         : 2024-01-17 14:01:18
+ * @LastEditors  : 郭恒海
+ * @LastEditTime : 2024-01-18 17:13:06
+ * @FilePath     : /app-pc/src-tauri/src/main.rs
+ * @Description  :
+ * guohenghai@126.com
+ */
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
@@ -8,8 +17,13 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
-    tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+    let app = tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![])
+        .build(tauri::generate_context!()) //notice build instead of run
+        .expect("error preparing tauri app");
+    app.run(|_app_handle, event| {
+        if let tauri::RunEvent::Updater(event) = event {
+            dbg!(event);
+        }
+    });
 }
