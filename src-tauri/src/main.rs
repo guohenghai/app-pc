7bd96264378c0a3227c469b6624fe1b6eb55bae3
guohenghai <guohenghai@126.com>
2024-01-19 08:27:26 +0800

    修改版本号去除 untagged

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,12 +1,3 @@
-/*
- * @Author       : 郭恒海
- * @Date         : 2024-01-17 14:01:18
- * @LastEditors  : 郭恒海
- * @LastEditTime : 2024-01-18 17:13:06
- * @FilePath     : /app-pc/src-tauri/src/main.rs
- * @Description  :
- * guohenghai@126.com
- */
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
@@ -17,13 +8,34 @@ fn greet(name: &str) -> String {
 }
 
 fn main() {
+    // 原来的代码
+    //tauri::Builder::default()
+    //    .invoke_handler(tauri::generate_handler![greet])
+    //    .run(tauri::generate_context!())
+    //    .expect("error while running tauri application");
+
+    // 新的带更新调试的代码
     let app = tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![])
         .build(tauri::generate_context!()) //notice build instead of run
         .expect("error preparing tauri app");
-    app.run(|_app_handle, event| {
-        if let tauri::RunEvent::Updater(event) = event {
-            dbg!(event);
+    //app.run(|_app_handle, event| {
+    //    if let tauri::RunEvent::Updater(event) = event {
+    //        dbg!(event);
+    //    }
+    //});
+
+    // 增强版本
+    app.run(|_app_handle, event| match event {
+        tauri::RunEvent::Ready => println!("Application is ready"),
+        tauri::RunEvent::Resumed => println!("Application is resumed"),
+        tauri::RunEvent::ExitRequested { .. } => println!("Exit was requested"),
+        tauri::RunEvent::Updater(event) => {
+            println!("Updater event: {:?}", event);
+            if let tauri::UpdaterEvent::Error(error) = event {
+                println!("Updater error details: {:?}", error);
+            }
         }
+        _ => {}
     });
 }
